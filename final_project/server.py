03dfc23def5a633c6455f2982887e3e18db779fd
Doheera-kosi <kupourevans99@gmail.com>
2023-05-02 13:44:29 +0000

    Modify server.py port

--- final_project/server.py
+++ final_project/server.py
@@ -1,7 +1,5 @@
-import machinetranslation
 from machinetranslation import translator
 from flask import Flask, render_template, request
-import json
 
 app = Flask("Web Translator")
 
@@ -22,4 +20,4 @@ def renderIndexPage():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
